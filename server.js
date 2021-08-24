1c85ef0e19febbd44a7e5a13badc1c14d5ade253
Cyber-Celestial <66738709+Cyber-Celestial@users.noreply.github.com>
2021-08-24 17:59:45 +0530

    First Commit

---
+++ server.js
@@ -0,0 +1,70 @@
+const express = require("express");
+const path = require("path");
+var app = express();
+const socket = require('socket.io');
+const port = process.env.PORT || 8080;
+//starting the server
+var server=app.listen(process.env.PORT || 5000,function(){
+    console.log("Listening on port ${port}`");
+  });
+
+const fs = require("fs");
+const fileUpload = require("express-fileupload");
+const io = require("socket.io")(server, {
+  allowEIO3: true, // false by default
+});
+app.use(express.static(path.join(__dirname, "")));
+var userConnections = [];
+io.on("connection", (socket) => {
+  console.log("socket id is ", socket.id);
+  socket.on("userconnect", (data) => {
+    console.log("userconnent", data.displayName, data.meetingid);
+    var other_users = userConnections.filter(
+      (p) => p.meeting_id == data.meetingid
+    );
+    userConnections.push({
+      connectionId: socket.id,
+      user_id: data.displayName,
+      meeting_id: data.meetingid,
+    });
+    var userCount = userConnections.length;
+    console.log(userCount);
+    other_users.forEach((v) => {
+      socket.to(v.connectionId).emit("inform_others_about_me", {
+        other_user_id: data.displayName,
+        connId: socket.id,
+        userNumber: userCount,
+      });
+    });
+    socket.emit("inform_me_about_other_user", other_users);
+  });
+  
+  socket.on("SDPProcess", (data) => {
+    socket.to(data.to_connid).emit("SDPProcess", {
+      message: data.message,
+      from_connid: socket.id,
+    });
+  });
+
+  socket.on("disconnect", function () {
+    console.log("Disconnected");
+    var disUser = userConnections.find((p) => p.connectionId == socket.id);
+    if (disUser) {
+      var meetingid = disUser.meeting_id;
+      userConnections = userConnections.filter(
+        (p) => p.connectionId != socket.id
+      );
+      var list = userConnections.filter((p) => p.meeting_id == meetingid);
+      list.forEach((v) => {
+        var userNumberAfUserLeave = userConnections.length;
+        socket.to(v.connectionId).emit("inform_other_about_disconnected_user", {
+          connId: socket.id,
+          uNumber: userNumberAfUserLeave,
+        });
+      });
+    }
+  });
+});
+
+
+
