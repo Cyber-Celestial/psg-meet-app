1fa4ddc4d6a5ca16eab0ab1f0c4a76790e994671
19z301 <wishnewk@gmail.com>
2021-10-27 00:17:57 +0530

    Update app.js

--- public/Assets/js/app.js
+++ public/Assets/js/app.js
@@ -642,14 +642,13 @@ var MyApp = (function () {
     var attachFileName = $("#customFile").val().split("\\").pop();
     var attachFilePath =
       "public/attachment/" + meeting_id + "/" + attachFileName;
-    attachFileArea.innerHTML +=
-      "<div class='left-align' style='display:flex; align-items:center;'><img src='public/assets/images/me2.jpg' style='height:40px;width:40px;' ><div style='font-weight:600;margin:0 5px;'>"
-      +
-      user_id +
+      attachFileAreaForOther.innerHTML +=
+      "<div class='left-align' style='display:flex; align-items:center;'><img src='public/Assets/images/other.jpg' style='height:40px;width:40px;' class='caller-image circle'><div style='font-weight:600;margin:0 5px;'>" +
+      data.username +
       "</div>:<div><a style='color:#007bff;' href='" +
-      attachFilePath +
+      data.filePath +
       "' download>" +
-      attachFileName +
+      data.fileName +
       "</a></div></div><br/>";
     $("label.custom-file-label").text("");
     socket.emit("fileTransferToOther", {
